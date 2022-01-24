791159cfa3c0b3045632018dad7a7487b5076519
Vincenzo Scalcione <54317749+vscalcione@users.noreply.github.com>
2022-01-24 09:56:34 +0100

    Initial commit

---
+++ src/test/java/it/vscalcione/springboot/demoapplication/SecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.demoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
